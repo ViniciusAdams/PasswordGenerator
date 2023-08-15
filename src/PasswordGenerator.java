788bd9d9a7027ccbd5f141565fd572cd08b39923
vinia <viniusadams98@gmail.com>
2023-08-15 21:38:55 +0100

    creating a simple and short functional password Generator using a foor loop,String Builder

---
+++ src/PasswordGenerator.java
@@ -0,0 +1,29 @@
+import java.security.SecureRandom;
+import java.util.Scanner;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the length of the password: ");
+        int passwordLength = scanner.nextInt();
+        scanner.close();
+
+        String generatedPassword = generatePassword(passwordLength);
+        System.out.println("Generated Password: " + generatedPassword);
+    }
+
+    public static String generatePassword(int length) {
+        final String charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()-_=+[]{}|;:,.<>?";
+        SecureRandom random = new SecureRandom();
+
+        StringBuilder password = new StringBuilder(length);
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(charset.length());
+            char randomChar = charset.charAt(randomIndex);
+            password.append(randomChar);
+        }
+
+        return password.toString();
+    }
+}
